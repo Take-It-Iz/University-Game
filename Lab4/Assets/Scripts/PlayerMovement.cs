0574a61e1e9713ffeb07f0c2e7465ab3814503a7
IZrunner <masterchefchinatown@gmail.com>
2021-03-08 00:35:13 +0200

    Lab 4

---
+++ Lab4/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 7f;
+    public float moveOnY = 0f;
+    public float moveOnZ = 0f;
+    public float jumpOnY = 5f;
+    public bool isFloor = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Move();
+    }
+
+    void Jump() 
+    {
+        if (Input.GetButtonDown("Jump") && isFloor == true) 
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpOnY), ForceMode2D.Impulse);
+        }        
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), moveOnY, moveOnZ);
+        transform.position += movement * Time.deltaTime * movementSpeed;
+    }
+}
---
+++ Lab4/Assets/Scripts/SFXTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXTrigger : MonoBehaviour
+{
+    public AudioSource hitSound;
+
+    void OnTriggerEnter2D(Collider2D playerCollider)
+    {
+        if (playerCollider.CompareTag("Player")) 
+		{
+            hitSound.Play();    
+		}
+    }
+}
