00f44bf62e61c5c7a54349d4cd59c30e7e7c1d19
IZrunner <masterchefchinatown@gmail.com>
2021-04-04 17:33:22 +0300

    Lab 6

---
+++ Lab6/Assets/Scripts/Launcher.cs
@@ -0,0 +1,91 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    private string gameVersion = "1";
+    /// Keep track of the current process. Since connection is asynchronous and is based on several callbacks from Photon,
+    /// we need to keep track of this to properly adjust the behavior when we receive call back by Photon.
+    /// Typically this is used for the OnConnectedToMaster() callback.
+    bool isConnecting;
+    public GameObject playerPrefab;
+    [SerializeField] private byte maxPlayersPerRoom = 2;
+    void Awake() 
+    {
+        // this makes sure we can use PhotonNetwork.LoadLevel() on the 
+        // master client and all clients in the same room sync their level automatically
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start()
+    {
+        if (playerPrefab == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",this);
+        }
+else
+{
+    Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
+    // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+    PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f,5f,0f), Quaternion.identity, 0);
+}
+    }
+
+    public void Connect()
+    {
+        // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+        if (PhotonNetwork.IsConnected)
+        {
+            // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            // #Critical, we must first and foremost connect to Photon Online Server.
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Launcher: OnConnectedToMaster() was called by PUN");
+        // The first we try to do is to join a potential existing room. 
+        // If there is, good, else, we'll be called back with OnJoinRandomFailed()
+        if (isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+            isConnecting = false;
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        isConnecting = false;
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("PUN Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+
+        // #Critical: We only load if we are the first player, else we rely on `PhotonNetwork.AutomaticallySyncScene` to sync our instance scene.        
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            Debug.Log("We load the 'Room for 1' ");
+
+            // #Critical
+            // Load the Room Level.
+            PhotonNetwork.LoadLevel("Room for 1");
+        }
+    }
+}
---
+++ Lab6/Assets/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,35 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameInputField : MonoBehaviour
+{
+    // Store the PlayerPref Key to avoid typos
+    const string playerNamePrefKey = "Nickname";
+    private void Start()
+    {
+        string defaultName = string.Empty;
+        InputField _inputField = this.GetComponent<InputField>();
+        if (_inputField!=null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                _inputField.text = defaultName;
+            }
+        }
+        PhotonNetwork.NickName =  defaultName;         
+    }
+
+    // Sets the name of the player, and save it in the PlayerPrefs for future sessions.
+    public void SetPlayerName(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                Debug.LogError("Nickname is null or empty");
+                return;
+            }
+            PhotonNetwork.NickName = value;
+            PlayerPrefs.SetString(playerNamePrefKey,value);
+        }
+}
