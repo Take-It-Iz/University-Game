3b90fe1939371c1fd3c1a378e29f883230d59fb6
IZrunner <masterchefchinatown@gmail.com>
2021-04-11 16:59:13 +0300

    Lab 7

---
+++ Lab7/Assets/Scripts/AnalyzerSample.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyzerSample : MonoBehaviour
+{
+    public void OnButtonPressed()
+    {
+        Analytics.CustomEvent("StartButtonPressed");
+    }
+}
---
+++ Lab7/Assets/Scripts/ConfigNameTitle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.RemoteConfig;
+using UnityEngine.UI;
+
+public class ConfigNameTitle : MonoBehaviour
+{
+    public struct userAttributes { }
+    public struct appAttributes { }
+
+    public string newNameTitle = "Input here:";
+    public Text title;
+ 
+    void Awake()
+    {
+        ConfigManager.FetchCompleted += SetNewNameTitle;
+        ConfigManager.FetchConfigs<userAttributes, appAttributes>(new userAttributes(), new appAttributes());
+    }
+
+    void SetNewNameTitle(ConfigResponse response)
+    {
+        newNameTitle = ConfigManager.appConfig.GetString("nameTitle");
+        title.text = newNameTitle;
+    }
+}
